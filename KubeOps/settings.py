f84912fa941aeec6f42c535746a1f7b3cddbf470
Heart Sea <39157554+xuqil@users.noreply.github.com>
2020-05-23 21:20:47 +0800

    Update settings.py
    解决因django版本出现的pymysql版本问题

--- KubeOps/settings.py
+++ KubeOps/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import sys
 import pymysql
+pymysql.version_info = (1, 3, 13, "final", 0)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
